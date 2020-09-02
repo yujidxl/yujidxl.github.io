992712fc1217217ccdd985e7af9b4e7e130e1bca
dengxiaolong <dengxiaolong@myhexin.com>
2020-09-02 11:49:29 +0800

    初始化blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+      {
+        text: 'Languages',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'Chinese', link: '/language/chinese/' },
+          { text: 'Japanese', link: '/language/japanese/' }
+        ]
+      }
+    ]
+  },
+  markdown: {
+    lineNumbers: true
+  }
+}
